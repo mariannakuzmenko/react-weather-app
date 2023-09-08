851c87b3e46cf9ed178ad99d792029ee007334ea
mariannakuzmenko <111392697+mariannakuzmenko@users.noreply.github.com>
2023-09-08 03:03:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
           </a>{" "}
           and is{" "}
           <a
-            href="https://github.com/wecodeschool/react-weather-app"
+            href="https://github.com/mariannakuzmenko/react-weather-app"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
+            href="https://weather1-app-react.netlify.app"
             target="_blank"
             rel="noopener noreferrer"
           >
